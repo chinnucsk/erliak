834b6673125209a668fefad36f3a3c793293148b
Nicklas Nordenmark <nordenmark@gmail.com>
2012-07-17 17:46:35 +0200

    additional tests for new api functions

--- test/erliak_tests.erl
+++ test/erliak_tests.erl
@@ -17,27 +17,45 @@
 %% 4. Run test_suite()
 
 %% Test values
+test_buckets() ->
+	[<<"b1">>, <<"b2">>, <<"b3">>].
 test_bucket() ->
     <<"b">>.
+test_keys() ->
+	[ list_to_binary("k" ++ integer_to_list(N)) || N <- lists:seq(1,9) ].
 test_key() ->
     <<"k">>.
 test_value() ->
     <<"v">>.
 
 %% sasl and ibrowse started before and stopped after every test, eh?
-setup() ->
+setup() ->	
+    application:set_env(sasl, sasl_error_logger, {file, "erliak_test_sasl.log"}),
+    error_logger:logfile({open, "erliak_test.log"}),
+    error_logger:tty(false),
+
     [ ok = application:start(A) || A <- [sasl, ibrowse] ],
+
+    %% application:start(ibrowse),
+    
     {ok, C} = erliak:start_link(),
     C.
 
-cleanup(C) ->
-    erliak:delete(test_bucket(), test_key()),
+cleanup(C) ->	    
+    % %% Delete test_bucket(), test_key()
+    % erliak:delete(test_bucket(), test_key()),    
+    % %% Delete test_key() from test_buckets()
+    % [ erliak:delete(B, test_key()) || B <- test_buckets() ],    
+    % %% Delete test_keys() from test_bucket()
+    % [ erliak:delete(test_bucket(), K) || K <- test_keys() ],
+	reset_riak(),
     erliak:stop(C),
     [ ok = application:stop(A) || A <- [sasl, ibrowse] ].
 
 %% The test suite that both clients should pass
 test_suite() ->
     Transport = atom_to_list(erliak_env:get_env(default_transport)),
+    reset_riak(),
     [{Transport ++ " - ping",
       { setup,
 	fun setup/0,
@@ -53,14 +71,97 @@ test_suite() ->
 	?_test( begin		  
 		    {ok, ServerInfo} = erliak:get_server_info(),
 		    [{node, _}, {server_version, _}] = lists:sort(ServerInfo)
-		end)}},	 
+		end)}},
+	 {Transport ++ " - get server info with timeout",
+      { setup,
+	fun setup/0,
+	fun cleanup/1,
+	?_test( begin		  
+		    {ok, ServerInfo} = erliak:get_server_info(500),
+		    [{node, _}, {server_version, _}] = lists:sort(ServerInfo)
+		end)}},		 
+	 {Transport ++ " - get client id",
+      { setup,
+	fun setup/0,
+	fun cleanup/1,
+	?_test( begin
+			{ok, ClientID} = erliak:get_client_id(),
+			%% Make sure it's a binary (PB) or a list of characters (HTTP)
+			case erliak_env:get_env(default_transport) of
+				http ->
+					?assertEqual(true, is_list(ClientID));
+				pb ->
+					?assertEqual(true, is_binary(ClientID))
+			end		    
+		end)}},
+	 {Transport ++ " - get client id with timeout",
+      { setup,
+	fun setup/0,
+	fun cleanup/1,
+	?_test( begin
+			{ok, ClientID} = erliak:get_client_id(500),
+			%% Make sure it's a binary (PB) or a list of characters (HTTP)
+			case erliak_env:get_env(default_transport) of
+				http ->
+					?assertEqual(true, is_list(ClientID));
+				pb ->
+					?assertEqual(true, is_binary(ClientID))
+			end		    
+		end)}},		 
+	 {Transport ++ " - empty list buckets with and without timeout",
+      { setup,
+	fun setup/0,
+	fun cleanup/1,
+	?_test( begin			
+			?assertEqual({ok, []}, erliak:list_buckets()),
+			?assertEqual({ok, []}, erliak:list_buckets(500))
+		end)}},	 	 
+	%% CHECK HOW CURRENT TESTS DO THIS
+	%% make sure to delete the bucket
+	 {Transport ++ " - put then list buckets with timeout and call timeout",
+      { setup,
+	fun setup/0,
+	fun cleanup/1,
+	?_test( begin
+			Buckets = test_buckets(),
+			F = fun(B) -> 
+				Obj = riakc_obj:new(B, test_key(), test_value()),
+				erliak:put(Obj)
+			end,
+			[ F(B) || B <- Buckets ],
+			{ok, BucketList} = erliak:list_buckets(500, 500),
+			?assertEqual(Buckets, lists:sort(BucketList))
+		end)}},	 	 
+	 {Transport ++ " - list keys in empty bucket with and without timeout",
+      { setup,
+	fun setup/0,
+	fun cleanup/1,
+	?_test( begin			
+			?assertEqual({ok, []}, erliak:list_keys(<<"very_empty_bucket">>)),
+			?assertEqual({ok, []}, erliak:list_keys(<<"very_empty_bucket">>, 500))		
+		end)}},
+	 {Transport ++ " - put keys then list keys",
+      { setup,
+	fun setup/0,
+	fun cleanup/1,
+	?_test( begin			
+			Keys = test_keys(),
+			F = fun(K) ->
+				Obj = riakc_obj:new(test_bucket(), K, test_value()),
+				erliak:put(Obj)
+			end,
+			[ F(K) || K <- Keys ],
+			{ok, KeyList} = erliak:list_keys(test_bucket()),
+			?assertEqual(Keys, lists:sort(KeyList))			
+		end)}},
      {Transport ++ " - simple put then read value of get",
       { setup,
 	fun setup/0,
 	fun cleanup/1,
 	?_test( begin
-		    O0 = riakc_obj:new(test_bucket(), test_key(), test_value()),
-		    ok = erliak:put(O0),
+			?assertEqual({error, notfound}, erliak:get(test_bucket(), test_key())),
+		    Obj = riakc_obj:new(test_bucket(), test_key(), test_value()),
+		    ok = erliak:put(Obj),
 		    {ok, GO} = erliak:get(test_bucket(), test_key()),
 		    ?assertEqual(test_value(), riakc_obj:get_value(GO))
 		end)}},
@@ -108,16 +209,6 @@ test_suite() ->
 		    {ok, GO} = erliak:get(test_bucket(), test_key(), [{r, 1}], 500),
 		    ?assertEqual(riakc_obj:get_contents(PO), riakc_obj:get_contents(GO))
 		end)}},
-     {Transport ++ " - put w. timeout and delete, ensure deleted",
-      { setup,
-	fun setup/0,
-	fun cleanup/1,
-	?_test( begin
-		    Obj = riakc_obj:new(test_bucket(), test_key(), test_value()),
-		    ok = erliak:put(Obj, 500),
-		    erliak:delete(test_bucket(), test_key()),
-		    ?assertEqual({error, notfound}, erliak:get(test_bucket(), test_key()))
-		end)}},
      {Transport ++ " - put and delete w. timeout, ensure deleted",
       { setup,
 	fun setup/0,
@@ -127,7 +218,17 @@ test_suite() ->
 		    ok = erliak:put(Obj),
 		    erliak:delete(test_bucket(), test_key(), 500),
 		    ?assertEqual({error, notfound}, erliak:get(test_bucket(), test_key()))
-		end)}}
+		end)}},
+	 {Transport ++ " - put w. timeout and delete, ensure deleted",
+      { setup,
+	fun setup/0,
+	fun cleanup/1,
+	?_test( begin
+		    Obj = riakc_obj:new(test_bucket(), test_key(), test_value()),
+		    ok = erliak:put(Obj, 500),
+		    erliak:delete(test_bucket(), test_key()),
+		    ?assertEqual({error, notfound}, erliak:get(test_bucket(), test_key()))
+		end)}}     
     ].
 
 switch_protocol_to_pb_test_() ->
@@ -153,3 +254,114 @@ switch_protocol_to_http_test_() ->
 
 http_test_() ->
     test_suite().
+
+%% ===================================================================
+%% Riak Resetting code from the pb client
+%% ===================================================================
+
+%% Get the riak version from the init boot script, turn it into a list
+%% of integers.
+riak_version() ->
+    StrVersion = element(2, rpc:call(test_riak_node(), init, script_id, [])),
+    [ list_to_integer(V) || V <- string:tokens(StrVersion, ".") ].
+
+%% Resets the riak node
+reset_riak() ->
+    %% sleep because otherwise we're going to kill the vnodes too fast
+    %% for the supervisor's maximum restart frequency, which will bring
+    %% down the entire node
+    ?assertEqual(ok, maybe_start_network()),
+    case riak_version() of
+        [1,Two|_] when Two >= 2->
+            reset_riak_12();
+        _ ->
+            reset_riak_legacy()
+    end.
+
+%% Resets a Riak 1.2+ node, which can run the memory backend in 'test'
+%% mode.
+reset_riak_12() ->
+    set_test_backend(),
+    ok = rpc:call(test_riak_node(), riak_kv_memory_backend, reset, []),
+    reset_ring().
+
+%% Sets up the memory/test backend, leaving it alone if already set properly.
+set_test_backend() ->
+    Env = rpc:call(test_riak_node(), application, get_all_env, [riak_kv]),
+    Backend = proplists:get_value(storage_backend, Env),
+    Test = proplists:get_value(test, Env),
+    case {Backend, Test} of
+        {riak_kv_memory_backend, true} ->
+            ok;
+        _ ->
+            ok = rpc:call(test_riak_node(), application, set_env, [riak_kv, storage_backend, riak_kv_memory_backend]),
+            ok = rpc:call(test_riak_node(), application, set_env, [riak_kv, test, true]),
+            Vnodes = rpc:call(test_riak_node(), riak_core_vnode_manager, all_vnodes, [riak_kv_vnode]),
+            [ ok = rpc:call(test_riak_node(), supervisor, terminate_child, [riak_core_vnode_sup, Pid]) ||
+                {_, _, Pid} <- Vnodes ]
+    end.
+
+%% Resets a Riak 1.1 and earlier node.
+reset_riak_legacy() ->
+    timer:sleep(500),
+    %% Until there is a good way to empty the vnodes, require the
+    %% test to run with ETS and kill the vnode master/sup to empty all the ETS tables
+    %% and the ring manager to remove any bucket properties
+    ok = rpc:call(test_riak_node(), application, set_env, [riak_kv, storage_backend, riak_kv_memory_backend]),
+
+    %% Restart the vnodes so they come up with ETS
+    ok = supervisor:terminate_child({riak_kv_sup, test_riak_node()}, riak_kv_vnode_master),
+    ok = supervisor:terminate_child({riak_core_sup, test_riak_node()}, riak_core_vnode_sup),
+    {ok, _} = supervisor:restart_child({riak_core_sup, test_riak_node()}, riak_core_vnode_sup),
+    {ok, _} = supervisor:restart_child({riak_kv_sup, test_riak_node()}, riak_kv_vnode_master),
+
+    %% Clear the MapReduce cache
+    ok = rpc:call(test_riak_node(), riak_kv_mapred_cache, clear, []),
+
+    %% Now reset the ring so bucket properties are default
+    reset_ring().
+
+%% Resets the ring to a fresh one, effectively deleting any bucket properties.
+reset_ring() ->
+    Ring = rpc:call(test_riak_node(), riak_core_ring, fresh, []),
+    ok = rpc:call(test_riak_node(), riak_core_ring_manager, set_my_ring, [Ring]).
+
+maybe_start_network() ->
+    %% Try to spin up net_kernel
+    os:cmd("epmd -daemon"),
+    case net_kernel:start([test_eunit_node(), longnames]) of
+        {ok, _} ->
+            erlang:set_cookie(test_riak_node(), test_cookie()),
+            ok;
+        {error, {already_started, _}} ->
+            ok;
+        X ->
+            X
+    end.
+
+%% Cookie for distributed erlang
+test_cookie() ->
+    case os:getenv("RIAK_TEST_COOKIE") of
+        false ->
+            'riak';
+        CookieStr ->
+            list_to_atom(CookieStr)
+    end.    
+
+%% Riak node under test - used to setup/configure/tweak it for tests
+test_riak_node() ->
+    case os:getenv("RIAK_TEST_NODE_1") of
+        false ->
+            'riak@127.0.0.1';
+        NodeStr ->
+            list_to_atom(NodeStr)
+    end.
+
+%% Node for the eunit node for distributed erlang
+test_eunit_node() ->
+    case os:getenv("RIAK_EUNIT_NODE") of
+        false ->
+            'eunit@127.0.0.1';
+        EunitNodeStr ->
+            list_to_atom(EunitNodeStr)
+    end.    
